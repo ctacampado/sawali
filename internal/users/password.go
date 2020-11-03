6560e10d0c44443a3f67f1d664231b8f1d8feff9
ctacampado <ct.acampado@gmail.com>
2020-11-03 16:40:08 +0800

    HashPassword and ComparePassword functions

--- internal/users/password.go
+++ internal/users/password.go
@@ -2,7 +2,6 @@ package users
 
 import (
 	"errors"
-	"log"
 	"sawali/pkg/toyerrs"
 
 	"golang.org/x/crypto/bcrypt"
@@ -16,40 +15,42 @@ func (a PwArgs) Len() int {
 	return len(a)
 }
 
-// HashAndSalt uses GenerateFromPassword to hash & salt pwd
-func HashAndSalt(pwd []byte) (string, *toyerrs.Error) {
-	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
-	if err != nil {
-		return "", toyerrs.New(
+// HashPassword uses GenerateFromPassword to hash & salt given password
+func HashPassword(pwd string) (string, *toyerrs.Error) {
+	err := &toyerrs.Error{}
+
+	bytes, e := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if e != nil {
+		err = toyerrs.New(
 			"users.ComparePasswords",
-			err,
+			e,
 			"PWDERR",
 			// asterisks instead of actual user password
 			// because we don't want to have it logged
 			// or stored
-			PwArgs{"*****"},
+			PwArgs{"****"},
 		)
 	}
 
-	return string(hash), nil
+	return string(bytes), err
 }
 
 // ComparePasswords compares the password input by the user
 // to the one that is stored in the DB.
-func ComparePasswords(hash string, plain string) *toyerrs.Error {
+func ComparePasswords(hash, pwd string) *toyerrs.Error {
+	err := &toyerrs.Error{}
 	bh := []byte(hash)
-	bp := []byte(plain)
+	bp := []byte(pwd)
 
-	err := bcrypt.CompareHashAndPassword(bh, bp)
-	if err != nil {
-		log.Println(err)
-		return toyerrs.New(
+	e := bcrypt.CompareHashAndPassword(bh, bp)
+	if e != nil {
+		err = toyerrs.New(
 			"users.ComparePasswords",
 			errors.New("invalid credentials"),
 			"PWDERR",
-			PwArgs{hash, plain},
+			PwArgs{hash, pwd},
 		)
 	}
 
-	return nil
+	return err
 }
